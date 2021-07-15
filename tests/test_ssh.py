c8f675e76922f8f02b9397278349d5a8ccffbb4e
nOOb3167 <nOOb3167@gmail.com>
2021-07-16 01:19:33 +0200

    stuff

--- tests/test_ssh.py
+++ tests/test_ssh.py
@@ -1,13 +1,14 @@
 import abc
-from asyncio import create_subprocess_exec, gather, new_event_loop, StreamReader, StreamWriter
-from asyncio.events import get_running_loop
+from asyncio import AbstractEventLoop, create_subprocess_exec, Future, gather, get_running_loop, new_event_loop, run_coroutine_threadsafe, StreamReader, StreamWriter
 from asyncio.subprocess import PIPE
+from collections.abc import Coroutine
 import contextlib
 import dataclasses
 import io
 import logging
 import socket
 from socket import AF_INET6, AF_UNSPEC, AI_ADDRCONFIG, AI_PASSIVE, AI_V4MAPPED, SOCK_STREAM, getaddrinfo
+from typing import Callable
 import pytest
 import threading
 from binascii import hexlify
@@ -97,6 +98,7 @@ class ThreadedClnt(threading.Thread, metaclass=abc.ABCMeta):
 
 
 class ThreadedClnt1(ThreadedClnt):
+    CLNT_TIMEOUT: float = 120
     addr: Addr
 
     def __init__(self, addr: Addr):
@@ -119,9 +121,9 @@ class ThreadedClnt1(ThreadedClnt):
             username='robey',
             port=self.addr.port,
             pkey=key,
-            timeout=5,
-            banner_timeout=5,
-            auth_timeout=5
+            timeout=self.CLNT_TIMEOUT,
+            banner_timeout=self.CLNT_TIMEOUT,
+            auth_timeout=self.CLNT_TIMEOUT
             )
 
         client.exec_command('blah blah')
@@ -144,20 +146,25 @@ class ServerCb(metaclass=abc.ABCMeta):
     def wait_exec(self, con_have_exec: threading.Event):
         pass
 
+    @abc.abstractmethod
     def communicate(self, chan: paramiko.Channel):
         pass
 
 
 class Server(paramiko.ServerInterface, metaclass=abc.ABCMeta):
     server_cb: ServerCb
+    con_have_chan_request: bool
     con_have_exec: threading.Event
 
     def __init__(self, server_cb: ServerCb):
         self.server_cb = server_cb
+        self.con_have_chan_request = False
         self.con_have_exec = threading.Event()
 
     def check_channel_request(self, kind: str, chanid: int):
-        return paramiko.OPEN_SUCCEEDED if kind == "session" else paramiko.OPEN_FAILED_ADMINISTRATIVELY_PROHIBITED
+        first_request = self.con_have_chan_request == False
+        self.con_have_chan_request = True
+        return paramiko.OPEN_SUCCEEDED if first_request and kind == "session" else paramiko.OPEN_FAILED_ADMINISTRATIVELY_PROHIBITED
 
     def get_allowed_auths(self, username: str):
         return "publickey"
@@ -237,6 +244,129 @@ def test_ssh(caplog):
         assert "Not enough fields found" not in caplog.text
         server_cb = ServerCb1(clnt)
         stuff(server_cb=server_cb, server_key=server_key, addr=addr)
+    assert 0
+
+
+class AsyncServ:
+    loop: AbstractEventLoop
+    s: socket.socket
+    server_key: paramiko.PKey
+
+    def __init__(self, s: socket.socket, server_key: paramiko.PKey):
+        self.loop = new_event_loop()
+        self.s = s
+        self.server_key = server_key
+    def __enter__(self):
+        return self
+    def run_until_complete(self):
+        self.loop.run_until_complete(self.start_())
+    async def start_(self):
+        while True:
+            nsock, addr = await get_running_loop().sock_accept(self.s)
+            set_nodelay(nsock)
+            print(f'got {nsock} | {addr}')
+            with paramiko.Transport(nsock) as t:
+                protocol_negotiation_future = get_running_loop().create_future()
+                con_have_exec_future = get_running_loop().create_future()
+                async def auth_publickey(username: str, key: paramiko.PKey):
+                    print(f'auth_publickey')
+                async def exec_request_pre(channel: paramiko.Channel, command: str):
+                    con_have_exec_future.set_result(None)
+                    print(f'exec_request_pre')
+                server = ServerX(
+                    con_have_exec=FutureEvent(con_have_exec_future),
+                    loop=get_running_loop(),
+                    coro_auth_publickey=auth_publickey,
+                    coro_exec_request_pre=exec_request_pre)
+                t.add_server_key(key=self.server_key)
+                t.start_server(event=FutureEvent(protocol_negotiation_future), server=server)
+                print(f'after_start')
+                await protocol_negotiation_future
+                print(f'after_negotiation')
+                await con_have_exec_future
+                print(f'after_have_exec')
+                return
+
+
+class FutureEvent:
+    future: Future
+
+    def __init__(self, future: Future):
+        self.future = future
+    
+    def set(self):
+        # loop.call_soon_threadsafe?
+        self.future.set_result(None)
+
+
+class ServerX(paramiko.ServerInterface, metaclass=abc.ABCMeta):
+    con_have_chan_request: bool
+    con_have_exec: FutureEvent
+    loop: AbstractEventLoop
+    coro_auth_publickey: Callable[[str, paramiko.PKey], Coroutine[None]]
+    coro_exec_request_pre: Callable[[paramiko.Channel, str], Coroutine[None]]
+
+    def __init__(self, con_have_exec: FutureEvent, loop: AbstractEventLoop, coro_auth_publickey: Callable[[str, paramiko.PKey], Coroutine[None]], coro_exec_request_pre: Callable[[paramiko.Channel, str], Coroutine[None]]):
+        self.con_have_chan_request = False
+        self.con_have_exec = con_have_exec
+        self.loop = loop
+        self.coro_auth_publickey = coro_auth_publickey
+        self.coro_exec_request_pre = coro_exec_request_pre
+
+    def check_channel_request(self, kind: str, chanid: int):
+        try:
+            return paramiko.OPEN_SUCCEEDED if not self.con_have_chan_request and kind == "session" else paramiko.OPEN_FAILED_ADMINISTRATIVELY_PROHIBITED
+        finally:
+            self.con_have_chan_request = True
+
+    def get_allowed_auths(self, username: str):
+        return "publickey"
+
+    def check_auth_publickey(self, username: str, key: paramiko.PKey):
+        try:
+            run_coroutine_threadsafe(self.coro_auth_publickey(username, key), loop=self.loop).result()
+            return paramiko.AUTH_SUCCESSFUL
+        except Exception:
+            return paramiko.AUTH_FAILED
+
+    def check_channel_exec_request(self, channel: paramiko.Channel, command: str):
+        try:
+            run_coroutine_threadsafe(self.coro_exec_request_pre(channel, command), loop=self.loop).result()
+            self.con_have_exec.set()
+            return True
+        except Exception:
+            return False
+
+
+def set_nodelay(s: socket.socket):
+    """https://docs.python.org/3/library/asyncio-eventloop.html#:~:text=The%20socket%20option%20TCP_NODELAY%20is%20set%20by%20default
+    FIXME: is this public API ? asyncio documentation hints on TCP_NODELAY being set by default"""
+    from asyncio.base_events import _set_nodelay
+    _set_nodelay(s) # FIXME: is this public API ?
+
+
+def sock_for_port_serv(port: int):
+    for family, typ, proto, canonname, sockaddr in getaddrinfo(None, port, family=AF_INET6, type=SOCK_STREAM, proto=0, flags=AI_PASSIVE | AI_V4MAPPED | AI_ADDRCONFIG):
+        with contextlib.suppress(Exception), \
+                contextlib.ExitStack() as es:
+            es.enter_context(contextlib.closing(s := socket.socket(family, typ | SOCK_NONBLOCK | SOCK_CLOEXEC, proto)))
+            s.bind(sockaddr)
+            s.listen(100)
+            es.pop_all()
+            return s
+    raise RuntimeError(f'Listening on port {port}')
+
+
+@pytest.mark.timeout(5)
+def test_ssh_2(caplog):
+    caplog.set_level(logging.INFO)
+    addr = Addr(host="localhost", port=5001)
+    with contextlib.closing(sock_for_port_serv(addr.port)) as sock:
+        with ThreadedClnt1(addr) as clnt:
+            clnt.con_begin.set()
+            server_key: paramiko.Ed25519Key = pkey_from_str(server_private_key)
+            serv = AsyncServ(sock, server_key)
+            serv.run_until_complete()
     assert 0
 
 
